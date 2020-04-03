10191b3ccbe1bd49e5d949ce5fdb8d8c9df22276
SeyedShobeiri <shobeiri@math.uh.edu>
2020-04-02 22:03:28 -0500

    hi

--- ShinyFinance.R
+++ ShinyFinance.R
@@ -74,6 +74,12 @@ hc_portfolio = hist(portfolio_returns_xts_rebalanced_monthly$returns, breaks = 5
 hchart(hc_portfolio,color = "cornflowerblue",name = "Portfolio") %>% hc_title(text = "Portfolio Returns Distribution") %>% 
   hc_add_theme(hc_theme_flat()) %>% hc_exporting(enabled = TRUE)
 
+portfolio_sd_xts_builtin = StdDev(asset_returns_xts,weights = w)
+portfolio_sd_xts_builtin_percent = round(portfolio_sd_xts_builtin*100,2)
+portfolio_sd_xts_builtin_percent[1,1]
+
+
+
 # -----------------------------------------------------------------
 
 # ----------------------- Tidyverse -------------------------------
@@ -131,6 +137,16 @@ portfolio_returns_tq_rebalanced_monthly %>% ggplot(aes(x=returns)) + geom_histog
   geom_density(alpha = 1,color = "red") +xlab("monthly returns") + ylab("distribution") + theme_update(plot.title = element_text(hjust = 0.5)) +
   ggtitle("Portfolio Histogram and Density")
 
+portfolio_sd_tidy_builtin_percent = portfolio_returns_dplyr_byhand %>% summarise(
+  sd = sd(returns),
+  sd_byhand = sqrt(sum((returns-mean(returns))^2)/(nrow(.)-1))) %>% mutate(dplyr = round(sd,4)*100,
+                                                                           dplyr_byhand = round(sd_byhand,4) *100)
+
+portfolio_sd_tidy_builtin_percent %>% select(dplyr,dplyr_byhand)
+
+
+
+
 
 
                                                                                    
@@ -152,8 +168,12 @@ portfolio_returns_dplyr_byhand %>% rename(tidyverse = returns) %>% mutate(equati
                                                                           xts = coredata(portfolio_returns_xts_rebalanced_monthly)) %>%
   mutate_if(is.numeric,funs(round(.,3))) %>% head(3)
 
+portfolio_sd_tidyquant_builtin_percent = portfolio_returns_tq_rebalanced_monthly %>% 
+  tq_performance(Ra = returns,Rb = NULL,performance_fun = table.Stats)%>%
+  select(Stdev) %>% mutate(tq_sd = round(Stdev,4)*100)
 
-
+portfolio_sd_tidy_builtin_percent %>% select(dplyr,dplyr_byhand) %>% mutate(xts_builtin = portfolio_sd_xts_builtin_percent,
+                                                                            matrix = sd_matrix_algebra_percent,tq = portfolio_sd_tidyquant_builtin_percent$tq_sd)
 
 # -------------------------------------------------------------------
 
@@ -189,6 +209,107 @@ portfolio_returns_byhand = (w_1*asset1) + (w_2*asset2) + (w_3*asset3) + (w_4*ass
 names(portfolio_returns_byhand) = "returns"
 
 portfolio_returns_byhand
+
+
+# ----------------------- Standard Deviation ---------------------------
+
+covariance_matrix = cov(asset_returns_xts)
+round(covariance_matrix,5)
+
+sd_matrix_algebra = sqrt(t(w) %*% covariance_matrix %*% w)
+
+sd_matrix_algebra_percent = round(sd_matrix_algebra * 100,2) %>% 'colnames<-'("standard deviation")
+sd_matrix_algebra_percent
+
+portfolio_returns_dplyr_byhand %>% ggplot(aes(x=date,y=returns)) + 
+  geom_point(color = "cornflowerblue") + scale_x_date(breaks = pretty_breaks(n=6)) +
+  ggtitle("Scatterplot of Returns by Date") + theme(plot.title = element_text(hjust = 0.5))
+
+sd_plot = sd(portfolio_returns_tq_rebalanced_monthly$returns)
+mean_plot = mean(portfolio_returns_tq_rebalanced_monthly$returns)
+
+portfolio_returns_tq_rebalanced_monthly %>% mutate(hist_col_red =
+                                                     if_else(returns < (mean_plot - sd_plot),
+                                                             returns,as.numeric(NA)),
+                                                   hist_col_green = 
+                                                     if_else(returns > (mean_plot + sd_plot),
+                                                             returns,as.numeric(NA)),
+                                                   hist_col_blue = 
+                                                     if_else(returns > (mean_plot - sd_plot) & returns < (mean_plot + sd_plot),
+                                                             returns,as.numeric(NA))) %>%
+  ggplot(aes(x=date)) + geom_point(aes(y=hist_col_red),color = "red") + geom_point(aes(y=hist_col_green),color = "green") +
+  geom_point(aes(y=hist_col_blue),color="blue") + geom_hline(yintercept = (mean_plot + sd_plot),color="purple",linetype = "dotted") +
+  geom_hline(yintercept = (mean_plot - sd_plot),color="purple",linetype="dotted") + labs(title = "Colored Scatter",y="monthly returns") +
+  scale_x_date(breaks = pretty_breaks(n=8)) + theme(plot.title = element_text(hjust=0.5))
+
+  asset_returns_long %>% group_by(asset) %>% summarize(sd=100*sd(returns)) %>% add_row(asset = "Portfolio",
+                                                                                       sd = portfolio_sd_tidy_builtin_percent$dplyr) %>% 
+    ggplot(aes(x=asset,y = sd,colour = asset)) + geom_point() + scale_y_continuous(labels = function(x) paste0(x,"%")) +
+    geom_text(aes(x = "Portfolio",y= portfolio_sd_tidy_builtin_percent$dplyr + .2),label = "Portfolio",color = "cornflowerblue")+
+    labs(y="Standard deviation")
+
+  
+  asset_returns_long %>%
+    group_by(asset) %>%
+    summarise(expected_return = mean(returns),
+              stand_dev = sd(returns)) %>%
+    add_row(asset = "Portfolio",
+            stand_dev =
+              sd(portfolio_returns_tq_rebalanced_monthly$returns),
+            expected_return =
+              mean(portfolio_returns_tq_rebalanced_monthly$returns)) %>%
+    ggplot(aes(x = stand_dev,
+               y = expected_return,
+               color = asset)) +
+    geom_point(size = 2) +
+    geom_text(
+      aes(x =
+            sd(portfolio_returns_tq_rebalanced_monthly$returns) * 1.11,
+          y =
+            mean(portfolio_returns_tq_rebalanced_monthly$returns),
+          label = "Portfolio")) +
+    ylab("expected return") +
+    xlab("standard deviation") +
+    ggtitle("Expected Monthly Returns versus Risk") +
+    scale_y_continuous(labels = function(x){ paste0(x, "%")}) +
+    # The next line centers the title
+    theme_update(plot.title = element_text(hjust = 0.5))
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
