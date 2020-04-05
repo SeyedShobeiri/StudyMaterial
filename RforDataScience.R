1b410894b646ec56a750c2a0f3769c3a4f95c8ad
SeyedShobeiri <shobeiri@math.uh.edu>
2020-04-05 10:59:28 -0500

    commit

--- RforDataScience.R
+++ RforDataScience.R
@@ -42,3 +42,123 @@ nz = map_data("nz")
 ggplot(nz,aes(long,lat,group = group)) + geom_polygon(fill = "white",color = "black")
 
 y=seq(1,10,length.out = 5)
+
+# ---------------------------------------- dplyr ------------------------------------
+
+library(nycflights13)
+library(tidyverse)
+attach(nycflights13)
+
+?nycflights13
+nycflights13::flights
+
+view(nycflights13::flights)
+
+filter(flights,month == 1, day == 1)
+
+(filter(flights,month == 1, day == 1))
+
+near(sqrt(2)^2,2)
+
+(filter(flights,month %in% c(11,12)))
+
+is.na(flights)
+
+x = tibble(m = c(1,NA,3))
+filter(x,x>1)
+filter(x,is.na(x)|x>2)
+
+
+arrange(flights,year,month,day)
+arrange(flights,desc(arr_delay))
+
+select(flights,year,month,day)
+select(flights,year:dep_time)
+select(flights,-c(year,month,day))
+
+
+rename(flights,tail_num = tailnum)
+
+select(flights,contains("TIME"))
+
+
+flights_sml = select(flights,year:day,ends_with("delay"),distance,air_time)
+mutate(flights_sml,gain = arr_delay - dep_delay,speed = distance/air_time * 60)
+
+transmute(flights,gain = arr_delay - dep_delay,hours = air_time/60,gain_per_hour = gain / hours)
+flights
+
+summarize(flights,delay = mean(dep_delay,na.rm = TRUE))
+
+by_day = group_by(flights,year,month,day)
+summarize(by_day,delay = mean(dep_delay,na.rm = TRUE))
+
+
+delays = flights %>% group_by(dest) %>% summarize(count = n(),dist = mean(distance,na.rm = TRUE),
+                                                  delay = mean(arr_delay,na.rm = TRUE)) %>% filter(count > 20,dest != "HNL")
+
+flights %>% group_by(year,month,day) %>% summarize(mean = mean(dep_delay,na.rm = TRUE))
+
+not_canceled = flights %>% filter(!is.na(dep_delay),!is.na(arr_delay))
+not_canceled %>% group_by(year,month,day) %>% summarize(mean = mean(dep_delay))
+
+delays = not_canceled %>% group_by(tailnum) %>% summarize(delay = mean(arr_delay), n = n())
+ggplot(data = delays,mapping = aes(x=n,y=delay)) + geom_point(alpha = 1.10) 
+
+delays %>% filter(n>25) %>% ggplot(mapping = aes(x=n,y=delay)) + geom_point(alpha=1/10)
+
+not_canceled %>% group_by(dest) %>% summarize(carriers = n_distinct(carrier)) %>% arrange(desc(carriers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
